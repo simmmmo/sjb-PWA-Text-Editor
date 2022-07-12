86b13a13890bdf93112588c9d7bd1cafc366f4f3
Simon Browne <70622234+simmmmo@users.noreply.github.com>
2022-07-12 11:49:09 +1000

    Update comments in js files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('GET from the database');
   // Create a connection to the database database and version we want to use.
@@ -29,7 +29,7 @@ export const putDb = async (content) => {
 
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Logic for a method that gets all the content from the database
 export const getDb = async () => {
     // Create a connection to the database database and version we want to use.
     const jateDb = await openDB('jate', 1);
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,7 +1,7 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     // Store the triggered events
     window.deferredPrompt = event;
@@ -11,7 +11,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
  
   const promptEvent = window.deferredPrompt;
@@ -30,7 +30,7 @@ butInstall.addEventListener('click', async () => {
 
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// Handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     // Clear prompt
     window.deferredPrompt = null;
